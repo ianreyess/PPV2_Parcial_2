cbe7d698bf4db5f61ad2a8c56f3b4fbab4b3f968
Ian Reyes <ianemmanuel780@outlook.com>
2024-02-14 21:30:25 -0600

    Ejerccio
    EjercicioCorregido

--- PPV2/Assets/Scripts/New Folder/MovimientoSimple.cs
+++ PPV2/Assets/Scripts/New Folder/MovimientoSimple.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoSimple : MonoAdvance
+public class MovimientoSimple : MonoBehaviour
 {
-    public int velocidad = 5f;
+    public float velocidad = 5f;
 
     void Update()
     {
         // Obtener la entrada del teclado
-        float movimientoHorizontal = Input.GetAxis("Horizontal"):
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
 
         // Calcular el vector de movimiento
-        Vector3 movimiento = new Vector(movimientoHorizontal, 0f, movimientoVertical);
+        Vector3 movimiento = new Vector3(movimientoHorizontal, movimientoVertical, 0f);
 
         // Mover el objeto en la dirección calculada
-        transform.Translate{ movimiento* velocidad *time.deltaTime, Space.World};
+        transform.Translate(movimiento * velocidad * Time.deltaTime, Space.World);
     }
 }
-}
+
